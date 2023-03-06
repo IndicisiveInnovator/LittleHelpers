e213403ddf69a7a1bf1ebde51a93fa5543cfc37f
IndicisiveInnovator <68562006+IndicisiveInnovator@users.noreply.github.com>
2023-03-06 00:03:27 -0500

    some comments and suggestions for changing the code
    instead of using a string buffer and a scanner, use buffered reader and fileReader implementation

--- Autoformatter/autoformat.java
+++ Autoformatter/autoformat.java
@@ -8,14 +8,24 @@ public class autoformat{
         String filepath = "format.txt";
         Scanner scan = new Scanner(new File(filepath));
         StringBuffer buffer = new StringBuffer();
+            
+        // use buffered reader and file reader instead of scanner and string buffer
+        FileReader read = new FileReader("format.txt");
+        BufferedReader br = new BufferedReader(read);
 
+/* TODO:  Remove this while loop and used buffered reader to scan through and 
+add contents to a string */
         while (scan.hasNextLine()) {
             //add everything from the file to the string buffer
           buffer.append(scan.nextLine()+ System.lineSeparator());
         }
 
+
         //combine everything in the buffer and turn it into a string
         String fileContents = buffer.toString();
+        //test to see if there are any contents in the buffer
+        //System.out.println("contents" + fileContents);
+
         scan.close(); //close the scanner cause we dont need it anymore
 
         //split all the contents of the string into an array
