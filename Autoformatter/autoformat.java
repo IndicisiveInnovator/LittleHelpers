e6e587ae423c2eabd6e68637c067c31bf7c6cd09
IndicisiveInnovator <68562006+IndicisiveInnovator@users.noreply.github.com>
2023-04-10 13:02:11 -0400

    changed program to use bufferedReader

--- Autoformatter/autoformat.java
+++ Autoformatter/autoformat.java
@@ -1,38 +1,42 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.File;
+import java.io.IOException;
 import java.util.Scanner;
+
 public class autoformat{
 
     public static void main (String args[]){
+        BufferedReader br = null;
+        File file = new File("Autoformatter/format.txt");
 
         try{
         String filepath = "format.txt";
-        Scanner scan = new Scanner(new File(filepath));
         StringBuffer buffer = new StringBuffer();
             
         // use buffered reader and file reader instead of scanner and string buffer
-        FileReader read = new FileReader("format.txt");
-        BufferedReader br = new BufferedReader(read);
+    
+        FileReader read = new FileReader(file);
+        br = new BufferedReader(read);
 
-/* TODO:  Remove this while loop and used buffered reader to scan through and 
-add contents to a string */
-        while (scan.hasNextLine()) {
-            //add everything from the file to the string buffer
-          buffer.append(scan.nextLine()+ System.lineSeparator());
+        String currLine;
+        while((currLine = br.readLine()) != null){
+            buffer.append(currLine + "\n" );
+            //System.out.println(currLine);
         }
 
-
-        //combine everything in the buffer and turn it into a string
+       //combine everything in the buffer and turn it into a string
         String fileContents = buffer.toString();
         //test to see if there are any contents in the buffer
-        //System.out.println("contents" + fileContents);
-
-        scan.close(); //close the scanner cause we dont need it anymore
+        System.out.println("contents" + fileContents);
 
         //split all the contents of the string into an array
         String[] array = fileContents.split("\n");
 
         //create this buffered writer to reformat the text file
-        BufferedWriter write = new BufferedWriter(new FileWriter("format.txt"));
+        BufferedWriter write = new BufferedWriter(new FileWriter("Autoformatter/format.txt"));
 
         //use this for loop to write and reformat the text file
         for(int i = 0; i< array.length;i++){
@@ -41,16 +45,20 @@ add contents to a string */
 ;
            
         }
-        write.close();//close the writer
+        write.close();//close the writer 
 
-    
-} catch(Exception e){
+} catch(IOException e){
+   e.printStackTrace();
+}catch(Exception e){
     e.getStackTrace();
+    } 
+finally{
+try{
+    if(br != null)
+        br.close();
+    } catch(IOException ex){
+        ex.printStackTrace();
+    }
 }
-
-
-
-
-
 }
 }
